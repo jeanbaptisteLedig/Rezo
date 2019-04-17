1bc52852b66af39d37e105bf3ca66cd3a14ce43a
Ledig <jeanbaptiste.ledig@ynov.com>
2019-04-17 18:24:52 +0200

    Add lastname for user model

---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,6 @@
+export const HOME = '/';
+export const SIGN_UP = '/register';
+export const SIGN_IN = '/login';
+export const PROFIL = '/profil';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
