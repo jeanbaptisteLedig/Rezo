781b2c120d6f216f03b69b115fa368dd93723f8b
Ledig <jeanbaptiste.ledig@ynov.com>
2019-05-10 12:25:06 +0200

    Correction

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-var cors = require('cors');
+const cors = require('cors')
 const config = require('./db');
 const users = require('./routes/user');
 const postroutes = require('./routes/postRoute');
 
 const app = express();
-
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/api/users', users);
+
 app.use('/api/posts', postroutes);
 
 app.get('/', function(req, res) {
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,22 @@
-// store.js
+//store.js
 
-import { createStore, applyMiddleware, compose } from 'redux';
+
+import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const inititalState = {};
 
+const middleware = applyMiddleware(
+    thunk
+);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
     inititalState,
-    compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+    composeEnhancers(middleware)
+);
 
 export default store;
